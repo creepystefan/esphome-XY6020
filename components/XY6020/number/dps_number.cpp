54b8229ede0ff8b6be04267c3bcf9914bec887c3
creepystefan <147045262+creepystefan@users.noreply.github.com>
2023-11-08 21:49:13 +0100

    Update dps_number.cpp

--- components/XY6020/number/dps_number.cpp
+++ components/XY6020/number/dps_number.cpp
@@ -1,13 +1,13 @@
-#include "dps_number.h"
+#include "xy6020_number.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
-namespace dps {
+namespace xy6020 {
 
-static const char *const TAG = "dps.number";
+static const char *const TAG = "xy6020.number";
 
-void DpsNumber::dump_config() { LOG_NUMBER("", "DPS Number", this); }
-void DpsNumber::control(float value) {
+void xy6020Number::dump_config() { LOG_NUMBER("", "xy6020 Number", this); }
+void xy6020Number::control(float value) {
   float resolution = 100.0f;
 
   if (this->holding_register_ == 0x0001) {
@@ -17,5 +17,5 @@ void DpsNumber::control(float value) {
   this->parent_->write_register(this->holding_register_, (uint16_t) (value * resolution));
 }
 
-}  // namespace dps
+}  // namespace xy6020
 }  // namespace esphome
