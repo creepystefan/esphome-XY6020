bea807615b8fe96910eb7e7d548eb18694e914f9
creepystefan <147045262+creepystefan@users.noreply.github.com>
2023-11-08 21:47:30 +0100

    Add files via upload

--- components/XY6020/__init__.py
+++ components/XY6020/__init__.py
@@ -1,87 +1,50 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import number
-from esphome.const import (
-    CONF_ENTITY_CATEGORY,
-    CONF_ICON,
-    CONF_ID,
-    CONF_MAX_VALUE,
-    CONF_MIN_VALUE,
-    CONF_MODE,
-    CONF_STEP,
-    CONF_UNIT_OF_MEASUREMENT,
-    ENTITY_CATEGORY_CONFIG,
-    ICON_EMPTY,
-    UNIT_AMPERE,
-    UNIT_VOLT,
-)
-
-from .. import CONF_DPS_ID, DPS_COMPONENT_SCHEMA, dps_ns
-
-DEPENDENCIES = ["dps"]
+from esphome.components import modbus
+from esphome.const import CONF_ID
 
 CODEOWNERS = ["@syssi"]
 
-DEFAULT_STEP = 1
+DEPENDENCIES = ["modbus"]
+AUTO_LOAD = ["binary_sensor", "number", "sensor", "switch", "text_sensor"]
+MULTI_CONF = True
 
-CONF_VOLTAGE_SETTING = "voltage_setting"
-CONF_CURRENT_SETTING = "current_setting"
+CONF_DPS_ID = "dps_id"
+CONF_CURRENT_RESOLUTION = "current_resolution"
 
-NUMBERS = {
-    CONF_VOLTAGE_SETTING: 0x0000,
-    CONF_CURRENT_SETTING: 0x0001,
+dps_ns = cg.esphome_ns.namespace("dps")
+Dps = dps_ns.class_("Dps", cg.PollingComponent, modbus.ModbusDevice)
+
+CurrentResolution = dps_ns.enum("CurrentResolution")
+CURRENT_RESOLUTION_OPTIONS = {
+    "AUTO": CurrentResolution.DPS_CURRENT_RESOLUTION_AUTO,
+    "LOW": CurrentResolution.DPS_CURRENT_RESOLUTION_LOW,
+    "HIGH": CurrentResolution.DPS_CURRENT_RESOLUTION_HIGH,
 }
 
-DpsNumber = dps_ns.class_("DpsNumber", number.Number, cg.Component)
-
-DPSNUMBER_SCHEMA = number.NUMBER_SCHEMA.extend(
+DPS_COMPONENT_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(DpsNumber),
-        cv.Optional(CONF_ICON, default=ICON_EMPTY): cv.icon,
-        cv.Optional(CONF_STEP, default=0.01): cv.float_,
-        cv.Optional(CONF_UNIT_OF_MEASUREMENT, default=UNIT_VOLT): cv.string_strict,
-        cv.Optional(CONF_MODE, default="BOX"): cv.enum(number.NUMBER_MODES, upper=True),
-        cv.Optional(
-            CONF_ENTITY_CATEGORY, default=ENTITY_CATEGORY_CONFIG
-        ): cv.entity_category,
+        cv.GenerateID(CONF_DPS_ID): cv.use_id(Dps),
     }
-).extend(cv.COMPONENT_SCHEMA)
+)
 
-CONFIG_SCHEMA = DPS_COMPONENT_SCHEMA.extend(
-    {
-        cv.Optional(CONF_VOLTAGE_SETTING): DPSNUMBER_SCHEMA.extend(
-            {
-                cv.Optional(CONF_MIN_VALUE, default=0.0): cv.float_,
-                cv.Optional(CONF_MAX_VALUE, default=50.0): cv.float_,
-            }
-        ),
-        cv.Optional(CONF_CURRENT_SETTING): DPSNUMBER_SCHEMA.extend(
-            {
-                cv.Optional(
-                    CONF_UNIT_OF_MEASUREMENT, default=UNIT_AMPERE
-                ): cv.string_strict,
-                cv.Optional(CONF_MIN_VALUE, default=0.0): cv.float_,
-                cv.Optional(CONF_MAX_VALUE, default=20.0): cv.float_,
-            }
-        ),
-    }
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(Dps),
+            cv.Optional(CONF_CURRENT_RESOLUTION, default="AUTO"): cv.enum(
+                CURRENT_RESOLUTION_OPTIONS, upper=True
+            ),
+        }
+    )
+    .extend(cv.polling_component_schema("5s"))
+    .extend(modbus.modbus_device_schema(0x01))
 )
 
 
 async def to_code(config):
-    hub = await cg.get_variable(config[CONF_DPS_ID])
-    for key, address in NUMBERS.items():
-        if key in config:
-            conf = config[key]
-            var = cg.new_Pvariable(conf[CONF_ID])
-            await cg.register_component(var, conf)
-            await number.register_number(
-                var,
-                conf,
-                min_value=conf[CONF_MIN_VALUE],
-                max_value=conf[CONF_MAX_VALUE],
-                step=conf[CONF_STEP],
-            )
-            cg.add(getattr(hub, f"set_{key}_number")(var))
-            cg.add(var.set_parent(hub))
-            cg.add(var.set_holding_register(address))
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await modbus.register_modbus_device(var, config)
+
+    cg.add(var.set_current_resolution(config[CONF_CURRENT_RESOLUTION]))
