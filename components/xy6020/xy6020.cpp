20434311a4dd7bf8ca2e4ab316b5b9bba3e4601f
creepystefan <147045262+creepystefan@users.noreply.github.com>
2023-11-23 22:09:00 +0100

    Update xy6020.cpp

--- components/xy6020/xy6020.cpp
+++ components/xy6020/xy6020.cpp
@@ -137,13 +137,13 @@ void xy6020::on_status_data_(const std::vector<uint8_t> &data) {
 //       Store M0
     
     //                    0x50     M0 Voltage Set
-  this->publish_state_(this->M0_50_sensor_, (float) xy6020_get_16bit(0x50) * 0.01f);    
+  this->publish_state_(this->M0_50_sensor_, (float) xy6020_get_16bit(0x80) * 0.01f);    
     //                0x51     M0 Current Set
-  this->publish_state_(this->M0_51_sensor_, (float) xy6020_get_16bit(0x51) * 0.01f);
+  this->publish_state_(this->M0_51_sensor_, (float) xy6020_get_16bit(0x81) * 0.01f);
     //                0x52    OVP Over Voltage Protect
-  this->publish_state_(this->M0_52_sensor_, (float) xy6020_get_16bit(0x52) * 0.01f);
+  this->publish_state_(this->M0_52_sensor_, (float) xy6020_get_16bit(0x82) * 0.01f);
     //                0x53    OCP Over Current Protect
-  this->publish_state_(this->M0_53_sensor_, (float) xy6020_get_16bit(0x53) * 0.01f);
+  this->publish_state_(this->M0_53_sensor_, (float) xy6020_get_16bit(0x83) * 0.01f);
     //                0x54    OPP
  // this->publish_state_(this->54_sensor_, (float) xy6020_get_16bit(0x54) * 0.01f);
     //                0x55    b_led2_set_max
