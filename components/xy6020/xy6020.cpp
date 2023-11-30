140fd2863915a9d247c6b31940543290e0907e1e
creepystefan <147045262+creepystefan@users.noreply.github.com>
2023-11-30 11:41:40 +0100

    Update xy6020.cpp

--- components/xy6020/xy6020.cpp
+++ components/xy6020/xy6020.cpp
@@ -148,7 +148,7 @@ void xy6020::on_status_data_(const std::vector<uint8_t> &data) {
  //   20     running Time  hours
   this->publish_state_(this->runtimehours_sensor_, (float) xy6020_get_16bit(20) * 1.f);    
  //   22     running Time  Minutes        
- // this->publish_state_(this->runtimeminutes_sensor_, (float) xy6020_get_16bit(22) * 0.1f);
+  this->publish_state_(this->runtimeminutes_sensor_, (float) xy6020_get_16bit(22) * 0.1f);
 
 
     
