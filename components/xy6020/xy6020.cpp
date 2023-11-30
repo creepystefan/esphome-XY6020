fd439dd9a44160ef0492904d9a5693b6a077d77d
creepystefan <147045262+creepystefan@users.noreply.github.com>
2023-11-30 09:33:35 +0100

    Update xy6020.cpp

--- components/xy6020/xy6020.cpp
+++ components/xy6020/xy6020.cpp
@@ -147,7 +147,7 @@ void xy6020::on_status_data_(const std::vector<uint8_t> &data) {
 //       Store M0
     
     //                    0x50     M0 Voltage Set
-  this->publish_state_(this->M0_50_sensor_, (float) xy6020_get_16bit(22)(24) * 0.1f);    
+  this->publish_state_(this->M0_50_sensor_, (float) xy6020_get_16bit(22) * 0.1f);    
     //                0x51     M0 Current Set
   this->publish_state_(this->M0_51_sensor_, (float) xy6020_get_16bit(24) * 0.1f);
     //                0x52    OVP Over Voltage Protect
