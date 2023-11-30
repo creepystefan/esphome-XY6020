90082b5ba794e19badc026090e36fdc4aa87e145
creepystefan <147045262+creepystefan@users.noreply.github.com>
2023-11-30 09:44:34 +0100

    Update xy6020.cpp

--- components/xy6020/xy6020.cpp
+++ components/xy6020/xy6020.cpp
@@ -20,7 +20,7 @@ static const char *const PROTECTION_STATUS[PROTECTION_STATUS_SIZE] = {
 };
 
 void xy6020::on_modbus_data(const std::vector<uint8_t> &data) {
-  if (data.size() == 58) {
+  if (data.size() == 64) {
     this->on_status_data_(data);
     return;
   }
