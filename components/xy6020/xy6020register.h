80e5ae3113dd69958cbd0ab896194797c85de8f9
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-11-13 01:17:28 +0100

    Update xy6020register.h

--- components/xy6020/xy6020register.h
+++ components/xy6020/xy6020register.h
@@ -9,24 +9,42 @@ static const float NO_DEC_UNIT = 1;
 static const float MULTIPLY_TEN_UNIT = 10;
 static const float MULTIPLY_THOUSAND_UNIT = 1000;
 
+
+  
+  
+ 
+#  AH_LOW: "Output AH low 16 bits"      # 0x06
+#  AH_HIGH: "Output AH high 16 bits"    # 0x07
+#  WH_LOW: "Output WH low 16 bits"      # 0x08
+#  WH_HIGH: "Output WH high 16 bits"    # 0x09
+#  OUT_H: "Open Time Hours"             # 0x0A
+#  OUT_M: "Open Time Minutes"           # 0x0B
+#  OUT_S: "Open Time Seconds"           # 0x0C
+  t_in: "internal Temperature"         # 0x0D
+  t_ex: "external Temperature"         # 0x0D   need external Sensor
+
+
 /* PHASE STATUS REGISTERS */
-static const uint16_t SELEC_TOTAL_ACTIVE_ENERGY = 0x0000;
-static const uint16_t SELEC_IMPORT_ACTIVE_ENERGY = 0x0002;
-static const uint16_t SELEC_EXPORT_ACTIVE_ENERGY = 0x0004;
-static const uint16_t SELEC_TOTAL_REACTIVE_ENERGY = 0x0006;
-static const uint16_t SELEC_IMPORT_REACTIVE_ENERGY = 0x0008;
-static const uint16_t SELEC_EXPORT_REACTIVE_ENERGY = 0x000A;
-static const uint16_t SELEC_APPARENT_ENERGY = 0x000C;
-static const uint16_t SELEC_ACTIVE_POWER = 0x000E;
-static const uint16_t SELEC_REACTIVE_POWER = 0x0010;
-static const uint16_t SELEC_APPARENT_POWER = 0x0012;
-static const uint16_t SELEC_VOLTAGE = 0x0014;
-static const uint16_t SELEC_CURRENT = 0x0016;
-static const uint16_t SELEC_POWER_FACTOR = 0x0018;
-static const uint16_t SELEC_FREQUENCY = 0x001A;
-static const uint16_t SELEC_MAXIMUM_DEMAND_ACTIVE_POWER = 0x001C;
-static const uint16_t SELEC_MAXIMUM_DEMAND_REACTIVE_POWER = 0x001E;
-static const uint16_t SELEC_MAXIMUM_DEMAND_APPARENT_POWER = 0x0020;
+//static const uint16_t SELEC_VOLTAGE_VALUE_SETTING = 0x00;
+//static const uint16_t SELEC_CURRENT_VALUE_SETTING = 0x01;
+static const uint16_t SELEC_VOLTAGE_VALUE_DISPLAY = 0x02;
+static const uint16_t SELEC_CURRENT_VALUE_DISPLAY = 0x03;
+static const uint16_t SELEC_POWER_OUTPUT_DISPLAY = 0x04;
+static const uint16_t SELEC_VOLTAGE_INPUT_DISPLAY = 0x05;
+static const uint16_t SELEC_ = 0x06;
+static const uint16_t SELEC_ = 0x07;
+static const uint16_t SELEC_ = 0x08;
+static const uint16_t SELEC_ = 0x09;
+static const uint16_t SELEC_ = 0x0A;
+static const uint16_t SELEC_ = 0x0B;
+static const uint16_t SELEC_ = 0x0C;
+static const uint16_t SELEC_ = 0x0D;
+static const uint16_t SELEC_ = 0x0E;
+static const uint16_t SELEC_ = 0x0F;
+static const uint16_t SELEC_ = 0x10;
+static const uint16_t SELEC_ = 0x11;
+static const uint16_t SELEC_ = 0x12;
+static const uint16_t SELEC_ = 0x13;
 
 }  // namespace xy6020
 }  // namespace esphome
