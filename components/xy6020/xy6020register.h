39da01131fecf75d29ceb762348f6b0301cdc2f7
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-12-10 22:44:13 +0100

    Update xy6020register.h

--- components/xy6020/xy6020register.h
+++ components/xy6020/xy6020register.h
@@ -16,20 +16,9 @@ static const uint16_t SELEC_VOLTAGE_VALUE_DISPLAY = 0x02;
 static const uint16_t SELEC_CURRENT_VALUE_DISPLAY = 0x03;
 static const uint16_t SELEC_POWER_OUTPUT_DISPLAY = 0x04;
 static const uint16_t SELEC_VOLTAGE_INPUT_DISPLAY = 0x05;
-static const uint16_t SELEC_ = 0x06;
-static const uint16_t SELEC_ = 0x07;
-static const uint16_t SELEC_ = 0x08;
-static const uint16_t SELEC_ = 0x09;
-static const uint16_t SELEC_ = 0x0A;
-static const uint16_t SELEC_ = 0x0B;
-static const uint16_t SELEC_ = 0x0C;
 static const uint16_t SELEC_TEMPERATUR_INTERNAL_DISPLAY = 0x0D;
 static const uint16_t SELEC_TEMPERATUR_EXTERNAL_DISPLAY = 0x0E;
-static const uint16_t SELEC_ = 0x0F;
-static const uint16_t SELEC_ = 0x10;
-static const uint16_t SELEC_ = 0x11;
-static const uint16_t SELEC_ = 0x12;
-static const uint16_t SELEC_ = 0x13;
+
 
 }  // namespace xy6020
 }  // namespace esphome
