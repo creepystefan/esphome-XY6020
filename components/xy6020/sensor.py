47be6c0543af1107622aedfa2d833719c47c428f
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-12-10 23:35:25 +0100

    Update sensor.py

--- components/xy6020/sensor.py
+++ components/xy6020/sensor.py
@@ -57,7 +57,7 @@ SENSORS = {
 }
 
 CONFIG_SCHEMA = (
-    cv.Schema({cv.GenerateID(): cv.declare_id(SelecMeter)})
+    cv.Schema({cv.GenerateID(): cv.declare_id(XY_6020)})
     .extend(
         {cv.Optional(sensor_name): schema for sensor_name, schema in SENSORS.items()}
     )
