f7124cc3d59ae531a8128f358f3e18c4682e5ad1
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-12-11 22:18:09 +0100

    Update sensor.py

--- components/xy6020/sensor.py
+++ components/xy6020/sensor.py
@@ -62,7 +62,7 @@ CONFIG_SCHEMA = (
     .extend(
         {cv.Optional(sensor_name): schema for sensor_name, schema in SENSORS.items()}
     )
-    cv.Schema({cv.GenerateID(): cv.declare_id(XY602050)})
+    #cv.Schema({cv.GenerateID(): cv.declare_id(XY602050)})
        
     .extend(cv.polling_component_schema("1s"))
     .extend(modbus.modbus_device_schema(0x01))
