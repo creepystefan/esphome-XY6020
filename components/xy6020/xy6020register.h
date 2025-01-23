e3b6f1e4c1819ac2451fe313fab00fc0830a5500
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-01-23 21:48:38 +0100

    Update xy6020register.h

--- components/xy6020/xy6020register.h
+++ components/xy6020/xy6020register.h
@@ -10,20 +10,20 @@ static const float MULTIPLY_TEN_UNIT = 10;
 static const float MULTIPLY_THOUSAND_UNIT = 1000;
  
 /* STATUS REGISTERS */
-static const uint16_t XY6020_SET_OUTPUT_VOLTAGE = 0x00;    //V_SET
-static const uint16_t XY6020_SET_OUTPUT_VOLTAGE = 0x01;    //C_SET
+//static const uint16_t XY6020_SET_OUTPUT_VOLTAGE = 0x00;    //V_SET
+//static const uint16_t XY6020_SET_OUTPUT_VOLTAGE = 0x01;    //C_SET
 static const uint16_t XY6020_OUTPUT_VOLTAGE = 0x02;        //VOUT
 static const uint16_t XY6020_OUTPUT_CURRENT = 0x03;        //IOUT
 static const uint16_t XY6020_OUTPUT_POWER = 0x04;          //POWER
 static const uint16_t XY6020_INPUT_VOLTAGE = 0x05;         //UIN
-static const uint16_t XY6020_KEY_LOCK = 0x0f;              //KEY LOCK
+//static const uint16_t XY6020_KEY_LOCK = 0x0f;              //KEY LOCK
 //  bit 0 = unlock
 //  bit 1 = locked
-static const uint16_t XY6020_PROTECT_STATE = 0x10;         //Protect_state
+//static const uint16_t XY6020_PROTECT_STATE = 0x10;         //Protect_state
 static const uint16_t XY6020_TEMPERATURE_INTERN = 0x0D;    //t_in
 static const uint16_t XY6020_TEMPERATURE_INTERN = 0x0D;    //t_ex
-static const uint16_t XY6020_OVERPOWER_PROTECT_VALUE = 0x55;    //0x55
-static const uint16_t XY6020_OVERTEMP_PROTECT_VALUE = 0x5c;    //0x55
+//static const uint16_t XY6020_OVERPOWER_PROTECT_VALUE = 0x55;    //0x55
+//static const uint16_t XY6020_OVERTEMP_PROTECT_VALUE = 0x5c;    //0x55
 
 
 
