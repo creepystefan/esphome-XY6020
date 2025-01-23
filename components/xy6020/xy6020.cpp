7b98682d3c1b578e4a8a3fdeabcfc7c63fc7c0bf
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-01-23 23:05:55 +0100

    Update xy6020.cpp

--- components/xy6020/xy6020.cpp
+++ components/xy6020/xy6020.cpp
@@ -25,7 +25,8 @@ void XY6020::on_modbus_data(const std::vector<uint8_t> &data) {
     memcpy(&f, &temp, sizeof(f));
     return (f * unit);
   };
-
+  float seted_output_voltage = xy6020_get_float(XY6020_SETED_OUTPUT_VOLTAGE * 2, NO_DEC_UNIT);         //0x00
+  float seted_output_current = xy6020_get_float(XY6020_SETED_OUTPUT_CURRENT * 2, NO_DEC_UNIT);         //0x01
   float output_voltage = xy6020_get_float(XY6020_OUTPUT_VOLTAGE * 2, NO_DEC_UNIT);         //0x02
   float output_current = xy6020_get_float(XY6020_OUTPUT_CURRENT * 2, NO_DEC_UNIT);         //0x03
   float output_power = xy6020_get_float(XY6020_OUTPUT_POWER * 2, NO_DEC_UNIT);             //0x04
@@ -34,7 +35,11 @@ void XY6020::on_modbus_data(const std::vector<uint8_t> &data) {
   float temperature_extern = xy6020_get_float(XY6020_TEMPERATURE_EXTERN * 2, NO_DEC_UNIT); //0x0e
   float model = xy6020_get_float(XY6020_MODEL * 2, NO_DEC_UNIT); //0x16
   float software_version = xy6020_get_float(XY6020_SOFTWARE_VERSION * 2, NO_DEC_UNIT); //0x17
-  
+
+  if (this->seted_output_voltage_sensor_ != nullptr)
+    this->seted_output_voltage_sensor_->publish_state(seted_output_voltage);               //0x00
+  if (this->seted_output_current_sensor_ != nullptr)
+    this->seted_output_current_sensor_->publish_state(seted_output_current);               //0x01
   if (this->output_voltage_sensor_ != nullptr)
     this->output_voltage_sensor_->publish_state(output_voltage);               //0x02
   if (this->output_current_sensor_ != nullptr)
