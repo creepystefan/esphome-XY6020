f8b246e98e4ae897ae37f763702ff56dc0787199
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-11 10:49:07 +0100

    Create xy6020register_number.h

---
+++ components/xy6020/number/xy6020register_number.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace esphome {
+namespace xy6020 {
+
+static const float TWO_DEC_UNIT = 0.01;
+static const float ONE_DEC_UNIT = 0.1;
+static const float NO_DEC_UNIT = 1;
+static const float MULTIPLY_TEN_UNIT = 10;
+static const float MULTIPLY_THOUSAND_UNIT = 1000;
+ 
+/* STATUS REGISTERS */
+//static const uint16_t XY6020_SET_OUTPUT_VOLTAGE = 0x00;    //V_SET
+//static const uint16_t XY6020_SET_OUTPUT_CURRENT = 0x01;    //C_SET
+
+    
+
+
+}  // namespace xy6020
+}  // namespace esphome
