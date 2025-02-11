a24c7303c28c4a1e485dc5981fb1b445120536a7
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-12 00:14:20 +0100

    Update __init__.py

--- components/xy6020/switch/__init__.py
+++ components/xy6020/switch/__init__.py
@@ -20,7 +20,7 @@ XY6020Switch = xy6020_ns.class_(
 )
 
 SWITCHES = {
-    CONF_TURN_ON_OFF: switch.swtich_schema(
+    CONF_TURN_ON_OFF: switch.switch_schema(
         device_class=DEVICE_CLASS_SWITCH,
     ), 
     CONF_KEY_PROTECT: swtich.switch_schema(
