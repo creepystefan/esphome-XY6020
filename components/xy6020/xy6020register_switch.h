ff54d8683be353301a366212d1b83cb4f76a0cec
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-11 10:37:51 +0100

    Create xy6020register_switch.h

---
+++ components/xy6020/xy6020register_switch.h
@@ -0,0 +1,18 @@
+
+#pragma once
+
+namespace esphome {
+namespace xy6020 {
+
+
+/* STATUS REGISTERS */
+
+//static const uint16_t XY6020_KEY_LOCK = 0x0f;              //KEY LOCK
+//  bit 0 = unlock
+//  bit 1 = locked
+//static const uint16_t XY6020_TURN_ON_OFF = 0x12;    // Power Turn ON or OFF
+//  bit 0 = OFF
+//  bit 1 = ON
+
+}  // namespace xy6020
+}  // namespace esphome
