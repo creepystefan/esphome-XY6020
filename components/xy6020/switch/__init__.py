66696790e3bb7662623cab675e76c393b8a46b82
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-02-12 09:25:48 +0100

    Update __init__.py

--- components/xy6020/switch/__init__.py
+++ components/xy6020/switch/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import switch
+from esphome.components import switch, modbus
 from esphome.const import (
     CONF_ID,
     DEVICE_CLASS_SWITCH,
